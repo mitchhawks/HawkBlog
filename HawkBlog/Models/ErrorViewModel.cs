5329ae2baa8269188b6ef12ae8d4f34e93f726b9
Mitchell Hawks <mitchhawks@hotmail.com>
2018-01-04 07:07:48 +0800

    Setup default project files

---
+++ HawkBlog/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HawkBlog.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
